147c175b4435857d4a9d847b2f8f15c8b8b07230
Titi <thibault.buathier@k-net.pro>
2018-01-09 10:26:30 +0100

    add findOneOrCreate function for models

--- app/models/address.js
+++ app/models/address.js
@@ -54,7 +54,19 @@ AddressSchema.methods = {
  * Statics
  */
 
-AddressSchema.statics = {
+AddressSchema.statics = = {
+  /**
+   * findOneOrCreate
+   *
+   * @param {Object} condition
+   * @param {Function} callback
+   * @api private
+   */
+  findOneOrCreate : function findOneOrCreate(condition, callback) {
+      const self = this
+      self.findOne(condition, (err, result) => {
+          return result ? callback(err, result) : self.create(condition, (err, result) => { return callback(err, result) })
+      })
 };
 
 mongoose.model('Address', AddressSchema);
--- app/models/group.js
+++ app/models/group.js
@@ -48,7 +48,19 @@ GroupSchema.methods = {
  * Statics
  */
 
-GroupSchema.statics = {
+GroupSchema.statics = = {
+  /**
+   * findOneOrCreate
+   *
+   * @param {Object} condition
+   * @param {Function} callback
+   * @api private
+   */
+  findOneOrCreate : function findOneOrCreate(condition, callback) {
+      const self = this
+      self.findOne(condition, (err, result) => {
+          return result ? callback(err, result) : self.create(condition, (err, result) => { return callback(err, result) })
+      })
 };
 
 mongoose.model('Group', GroupSchema);
