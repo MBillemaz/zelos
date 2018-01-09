45f136dc83c9760d1486d20e8a641f64ae6c4ce2
Titi <thibault.buathier@k-net.pro>
2018-01-09 11:13:20 +0100

    fix load default data

--- index.js
+++ index.js
@@ -12,35 +12,5 @@ fs.readdirSync(models)
   .filter(file => ~file.search(/^[^\.].*\.js$/))
   .forEach(file => require(join(models, file)));
 
-const Group = mongoose.model('Group');
-var newGroup = Group.findOneOrCreate({
-    label: 'administrateur',
-    description: 'Les administrateurs'
-});
-
-const AddressType = mongoose.model('AddressType');
-var newAddressType = AddressType.findOneOrCreate({
-    label: 'livraison'
-});
-
-const Address = mongoose.model('Address');
-var newAddress = Address.findOneOrCreate({
-    number: 3,
-    street: "bobby",
-    zip_code: "01700",
-    city: "Lyon",
-    country: "Chine",
-    email: "bob@mail.com",
-    phone: "0649634976",
-});
-
-const User = mongoose.model('User');
-var newUser = User.findOneOrCreate({
-    name: 'tsointsoin',
-    first_name: 'tagada',
-    birth_date: new Date(1657, 01, 25),
-    login: 'a',
-    password: 'pouetpouet',
-    groups: newGroup,
-    addresses: newAddress
-});
+/* On charge les valeurs par défaults */
+require(join(__dirname, 'app/utils/defaultData.js'))
